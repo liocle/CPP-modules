c426f8f2690a86a30853bc978798fc4c49bf0e88
Lionel Clerc <lionel.clerc@protonmail.com>
2023-11-27 15:17:30 +0200

    test some lunar vim features

---
+++ Module_01/ex01/Zombie.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.hpp"
+
+
+Zombie::Zombie (std::string zombie_name) : _name(zombie_name) {
+}
+
+Zombie::~Zombie(void) {
+    std::cout << "\t🧟 " + this->_name + " goes back to where it belongs...⚰️" << std::endl;
+}
+
+void Zombie::announce(void) {
+    std::cout << "\t🧠 " + this->_name + ": BraiiiiiiinnnzzzZ🥤..." << std::endl;
+}  
