12e98512f1b1b862fd5c212ae9597282ea78b312
Lionel Clerc <lionel.clerc@protonmail.com>
2023-11-28 14:19:23 +0200

    Added colours to Module01 Exercise 02

--- Module_01/ex02/main.cpp
+++ Module_01/ex02/main.cpp
@@ -5,6 +5,7 @@
 #define RESET   "\033[0m"
 #define BLUE    "\033[34m"
 #define GREEN   "\033[32m"
+#define TOTO    "\033[36m"
 
 int main() { 
 
@@ -17,15 +18,15 @@ int main() {
     std::cout << std::endl;
     std::cout << "Declaration and initialization done as:" << std::endl;
     std::cout << "\tstd::string " << BLUE << "string      = \"HI THIS IS BRAIN\";" << RESET << std::endl;
-    std::cout << "\tstd::string *stringPTR  = &string;" << std::endl;
-    std::cout << "\tstd::string &stringREF  = string;" << std::endl;
+    std::cout << "\tstd::string " << TOTO << "*stringPTR  = \"&string\";" << RESET << std::endl;
+    std::cout << "\tstd::string " << GREEN << "&stringREF  = \"string\";" << RESET << std::endl;
     
     std::cout << std::endl;
     
     std::cout << "Syntax to obtain address:\n";
     std::cout << "\tstd::cout "<< BLUE << "<< \"Address of string:\t\"<< &string " << RESET << "<< std::endl;" << std::endl;
-    std::cout << "std::cout << \"Address of stringPTR:\t\"<< stringPTR << std::endl;" << std::endl;
-    std::cout << "std::cout << \"Address of stringREF:\t\"<< &stringREF << std::endl;" << std::endl;
+    std::cout << "\tstd::cout "<< TOTO << "<< \"Address of stringPTR:\t\"<< stringPTR " << RESET << "<< std::endl;" << std::endl;
+    std::cout << "\tstd::cout "<< GREEN << "<< \"Address of stringREF:\t\"<< &stringREF " << RESET << "<< std::endl;" << std::endl;
     std::cout << std::endl;
     std::cout << "Address of string:\t"<< &string << std::endl;
     std::cout << "Address of stringPTR:\t"<< stringPTR << std::endl;
@@ -34,7 +35,9 @@ int main() {
     std::cout << std::endl;
 
     std::cout << "Syntax to get value:\n";
-    std::cout << "\tstd::cout << \"Value of string:\t\t\t\"<< string << std::endl;\n \tstd::cout << \"Value pointed at by stringPTR:\t\" << *stringPTR << std::endl;\n \tstd::cout << \"Value referred by stringREF:\t\"<< stringREF << std::endl;\n" << std::endl;
+    std::cout << "\tstd::cout " << BLUE << "<< \"Value of string:\t\t\t\"<< string " << RESET << "<< std::endl;" << std::endl;
+    std::cout << "\tstd::cout " << TOTO << "<< \"Value pointed at by stringPTR:\t\" << *stringPTR " << RESET << " << std::endl;" << std::endl;
+    std::cout << "\tstd::cout " << GREEN << "<< \"Value referred by stringREF:\t\"<< stringREF  " << RESET << "<< std::endl;\n" << std::endl;
     
     std::cout << "Value of string:\t\t"<< string << std::endl;
     std::cout << "Value pointed at by stringPTR:\t" << *stringPTR << std::endl;
