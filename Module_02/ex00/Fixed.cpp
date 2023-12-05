7b5e6b48cace9af10d83e5421e0cda81a16f71a5
Lionel Clerc <lionel.clerc@protonmail.com>
2023-12-05 17:19:15 +0200

    Added comments

--- Module_02/ex00/Fixed.cpp
+++ Module_02/ex00/Fixed.cpp
@@ -1,32 +1,61 @@
 #include "Fixed.hpp"
 
+/**
+ * @brief Default constructor.
+ * Initializes the value to 0.
+ */
 Fixed::Fixed() : _value(0) {
-    std::cout << BLUE << " -- Default constructor called, initialized _value to to 0 -- " << RESET << std::endl;
+    std::cout << BLUE << " -- Default constructor called, initialized _value to 0 -- " << RESET << std::endl;
 }
 
+/**
+ * @brief Copy constructor.
+ * Initializes the value with the source object's value.
+ *
+ * @param source The source object to copy.
+ */
 Fixed::Fixed(const Fixed &source) : _value(source._value) {
     std::cout << CYAN << " -- Copy constructor called, source's _value set to copied instance's value -- " << RESET << std::endl;
 }
 
+/**
+ * @brief Destructor.
+ */
 Fixed::~Fixed() {
     std::cout << " -- Destructor called -- " << std::endl;
 }
 
+/**
+ * @brief Overloaded assignment operator.
+ * Copies the value from the source object.
+ *
+ * @param source The source object to copy.
+ * @return Reference to the current object.
+ */
 Fixed &Fixed::operator=(const Fixed &source) {
     std::cout << GREEN << " -- Copy instance via operator overload -- " << RESET << std::endl;
     if (this != &source) {
-        this->_value    = source._value;
+        this->_value = source._value;
     }
     return *this;
 }
 
-int     Fixed::getRawBits() const {
+/**
+ * @brief Getter function for raw bits.
+ * 
+ * @return The raw bit value of the fixed-point number.
+ */
+int Fixed::getRawBits() const {
     std::cout << " -- getRawbits member function called: ";
     return _value;
 }
 
-void    Fixed::setRawBits(int const raw) {
+/**
+ * @brief Setter function for raw bits.
+ * 
+ * @param raw The raw bit value to set.
+ */
+void Fixed::setRawBits(int const raw) {
     std::cout << " -- setRawBits member function called" << std::endl;
     _value = raw;
 }
-
--- Module_02/ex00/Fixed.hpp
+++ Module_02/ex00/Fixed.hpp
@@ -10,21 +10,27 @@
 # define CYAN "\033[36m"
 # define RED  "\033[31m"
 
-
+/**
+ * @brief Class representing a fixed-point number.
+ * 
+ * This class follows the canonical form for a C++ class, including a default
+ * constructor, a copy constructor, an assignment operator overload, and a destructor.
+ * It also provides getter and setter functions for raw bit manipulation.
+ */
 class Fixed {
 public:
-    Fixed(void);
-    Fixed(const Fixed &source);
-    ~Fixed(void);
+    Fixed(void);                                // Default Constructor.
+    Fixed(const Fixed &source);                 // Copy constructor.
+    ~Fixed(void);                               // Destructor. 
+    Fixed   &operator = (const Fixed &source);  // Overloaded assignment operator.
     
-    int     getRawBits(void) const;
-    void    setRawBits(int const raw);
+    int     getRawBits(void) const;             // Getter function for the raw bits.
+    void    setRawBits(int const raw);          // Setter function for the raw bits.
     
-    Fixed   &operator = (const Fixed &source);
 
 private:
-    int                 _value;
-    static const int    _fractionalBits = 8;
+    int                 _value;                 // Integer to store the value of the fixed-point number.
+    static const int    _fractionalBits = 8;    // Number of bits in the fractional part.
 };
 
 #endif
