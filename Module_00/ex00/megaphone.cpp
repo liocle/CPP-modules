491b1776dc14e4827316ac134425d3b0d38e5103
Lionel Clerc <lionel.clerc@protonmail.com>
2023-12-06 14:03:56 +0200

    fixed Makefile Module00 ex00

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -1,25 +1,24 @@
 #include <cctype>
 #include <iostream>
+#include <iostream>
 
-int	main (int argc, char **argv)
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else 
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			std::string argument(argv[i]);
-			for (int j = 0; argument[j] != '\0'; j++) {
-				char &c = argument[j];
-				c = std::toupper(c);
-			}
-		std::cout << argument;
-		}
-	}
-	std::cout << std::endl;
-	return (0);
+int main(int argc, char **argv) {
 
+  if (argc == 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  else {
 
+    for (int i = 1; i < argc; i++) {
+
+      std::string argument(argv[i]);
+      for (int j = 0; argument[j] != '\0'; j++) {
+
+        char &c = argument[j];
+        c = std::toupper(c);
+      }
+      std::cout << argument;
+    }
+  }
+  std::cout << std::endl;
+  return (0);
 }
-
