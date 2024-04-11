a7d747507564568d7add24c26da6188c2d20c39e
Lionel Clerc <lionel.clerc@protonmail.com>
2024-04-11 13:53:41 +0300

    Module07-ex00: Add student struct comparison

--- Module_07/ex00/main.cpp
+++ Module_07/ex00/main.cpp
@@ -1,11 +1,26 @@
 #include "whatever.hpp"
 #include <iostream>
 
+struct Student {
+  std::string name;
+  int grade;
+
+  bool operator<(const Student &rhs) const { return (this->grade < rhs.grade); }
+  bool operator>(const Student &rhs) const { 
+    return (this->grade > rhs.grade); }
+};
+
+std::ostream &operator<<(std::ostream &os, const Student &student) {
+  os << student.name << " is graded: " << student.grade;
+  return os;
+};
+
 int main(void) {
   /**
-   * The double colons :: in C++ are known as the scope resolution operator. 
-   * When used before a function or variable name like ::swap(a, b);, it explicitly
-   * instructs the compiler to use the global version of the function or variable.
+   * The double colons :: in C++ are known as the scope resolution operator.
+   * When used before a function or variable name like ::swap(a, b);, it
+   * explicitly instructs the compiler to use the global version of the function
+   * or variable.
    */
   {
     // SWAP TEMPLATE //
@@ -50,8 +65,14 @@ int main(void) {
 
     std::cout << "Initial values:\nstr_1: " << str_1 << "\nstr_2: " << str_2
               << std::endl;
+    std::cout << "Min value is: " << ::min(str_1, str_2) << std::endl << std::endl;
 
-    std::cout << "Min value is: " << ::min(str_1, str_2) << std::endl;
+    Student toto{"t0t0_1", 0};
+    Student tata{"t0t0_2", 20};
+    std::cout << "Student 1 data: " << toto << std::endl;
+    std::cout << "Student 2 data: " << tata << std::endl;
+    std::cout << "Min student grade is: " << ::min(toto, tata).grade
+              << std::endl;
   }
 
   {
@@ -63,7 +84,7 @@ int main(void) {
     std::cout << "Initial values:\nint_1: " << int_1 << "\nint_2: " << int_2
               << std::endl;
 
-    std::cout << "max value is: " << ::max(int_1, int_2) << std::endl;
+    std::cout << "max value is: " << ::max(int_1, int_2) << std::endl << std::endl;
 
     std::cout << "\nUsing the max template with a different type:\n"
               << std::endl;
@@ -73,7 +94,14 @@ int main(void) {
     std::cout << "Initial values:\nstr_1: " << str_1 << "\nstr_2: " << str_2
               << std::endl;
 
-    std::cout << "max value is: " << ::max(str_1, str_2) << std::endl;
+    std::cout << "max value is: " << ::max(str_1, str_2) << std::endl << std::endl;;
+
+    Student toto{"t0t0_1", 0};
+    Student tata{"t0t0_2", 20};
+    std::cout << "Student 1 data: " << toto << std::endl;
+    std::cout << "Student 2 data: " << tata << std::endl;
+    std::cout << "Max student grade is: " << ::max(toto, tata).grade
+              << std::endl;
   }
   {
     std::cout << "\n_____SUBJECT MAIN_____" << std::endl;
--- Module_07/ex00/whatever.hpp
+++ Module_07/ex00/whatever.hpp
@@ -7,11 +7,9 @@ template<typename T> void swap(T &swap_1, T &swap_2) {
   swap_2 = temp;
 }
 
-
 template<typename T> T const & min(const T& min_1, const T& min_2) {
     return (min_1 < min_2 ? min_1 : min_2);
 }
-
 
 template<typename T> T const & max(const T& max_1, const T& max_2) {
     return (max_1 > max_2 ? max_1 : max_2);
