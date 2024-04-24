27a8a91bf230ec75ffaccd6eae8bbe3f6c9c2ad3
Lionel Clerc <lionel.clerc@protonmail.com>
2024-04-24 18:47:21 +0300

    Module08_ex02: WIP

--- Module_08/ex02/MutantStack.hpp
+++ Module_08/ex02/MutantStack.hpp
@@ -1,23 +1,57 @@
+/**
+ * @file MutantStack.hpp
+ * @brief Extends std::stack to provide iterator support.
+ *
+ * The MutantStack class is a template class that extends the std::stack
+ * container adapter by enabling iteration over its elements. It inherits
+ * from std::stack while using std::deque as the underlying container.
+ *
+ * @tparam T Type of the elements.
+ */
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
 #include <deque>
 #include <stack>
 
+/**
+ * @class MutantStack
+ * @brief Extends std::stack by adding iterator capabilities.
+ *
+ * This class provides a stack with the added ability to be iterated over,
+ * something not possible with the standard std::stack. It exposes iterators
+ * of the underlying deque container, allowing both forward and reverse iteration.
+ *
+ * @tparam T Specifies the type of the elements stored in the stack.
+ */
 template <typename T>
 class MutantStack : public std::stack<T, std::deque<T>> {
-   public:
-    using std::stack<T, std::deque<T>>::stack;  // inherit constructors
+   private:
+    MutantStack(const MutantStack& source) = delete; // Copy constructor deleted to prevent unintended duplication of the container's content
+    MutantStack operator=(const MutantStack& source) = delete; // Assignement operator deleted to avoid copying
 
+   public:
+    MutantStack() {};
+    ~MutantStack() {};    
+    /**
+     * @brief Inherit constructors from the base std::stack class.
+     *
+     * Utilizes the constructor of the std::stack<T, std::deque<T>>,
+     * allowing initialization using the same parameters as the underlying deque.
+     */
+    using std::stack<T, std::deque<T>>::stack;  
+
+    // Iterator typdefs
     typedef typename std::deque<T>::iterator iterator;
     typedef typename std::deque<T>::const_iterator const_iterator;
     typedef typename std::deque<T>::reverse_iterator reverse_iterator;
     typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
 
-    iterator begin() { return this->c.begin(); }
-    iterator end() { return this->c.end(); }
+    // Returns const/-iterators to the beginning/end of the reversed/-stack
+    iterator begin() { return this->c.begin(); }    // Returns an iterator to the beginning of the stack
+    iterator end() { return this->c.end(); }        // Returns an iterator to the end of the stack 
 
-    const_iterator begin() const { return this->c.begin(); }
+    const_iterator begin() const { return this->c.begin(); } 
     const_iterator end() const { return this->c.end(); }
 
     reverse_iterator rbegin() { return this->c.rbegin(); }
@@ -26,5 +60,4 @@ class MutantStack : public std::stack<T, std::deque<T>> {
     const_reverse_iterator rbegin() const { return this->c.rbegin(); }
     const_reverse_iterator rend() const { return this->c.rend(); }
 };
-
 #endif  // MUTANTSTACK_HPP
--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -1,48 +1,55 @@
 #include <iostream>
+#include "Colors.h"
 #include "MutantStack.hpp"
 
-void displayMstackInfo(MutantStack<int>& mstack, std::string description) {
-    std::cout << "\n-- Mutant stack's info -- " + description + " " << std::endl;
+template <typename T>
+void displayMstackInfo(MutantStack<T>& mstack, std::string description) {
+    std::cout << CYAN << "-- Mutant stack's info -> " << RESET << description + " " << std::endl;
     if (!mstack.empty()) {
-        std::cout << "Mutant stack top element: " << mstack.top() << std::endl;
-        std::cout << "Mutant stack size: " << mstack.size() << std::endl;
-        std::cout << "Mutant stack content: ";
-        for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it) {
+        std::cout << CYAN << "   Top element:\t" << RESET << mstack.top() << std::endl;
+        std::cout << CYAN << "   Size:\t" << RESET << mstack.size() << std::endl;
+        std::cout << CYAN << "   Content:\t" << RESET;
+        for (typename MutantStack<T>::iterator it = mstack.begin(); it != mstack.end(); ++it) {
             std::cout << *it << " ";
         }
         std::cout << std::endl;
     } else {
-        std::cout << "Mutant stack is empty, size: " << mstack.size() << std::endl;
+        std::cout << std::boolalpha << CYAN << "   Empty:\t" << GREEN << mstack.empty() << RESET << std::endl;
     }
 }
 
 // Function to test basic stack operations
 void testStackOperations() {
+    std::cout << YELLOW << "Testing stack operations..." << RESET << std::endl;
     MutantStack<int> stack;
-    std::cout << "Testing stack operations..." << std::endl;
+    displayMstackInfo(stack, "Instanciating an empty stack");
 
     // Push elements
     stack.push(10);
-    stack.push(20);
+    displayMstackInfo(stack, "push 10 to stack");
+    stack.emplace(20);
+    displayMstackInfo(stack, "emplace 20 to stack");
     stack.push(30);
-    displayMstackInfo(stack, "Initial stack");
-
-    // Check top element
-    // assert(stack.top() == 30);
+    displayMstackInfo(stack, "push 30 to stack");
 
     // Pop elements and check LIFO order
     stack.pop();
-    displayMstackInfo(stack, "poped top value.");
+    displayMstackInfo(stack, "pop top value (LIFO)");
     stack.pop();
-    displayMstackInfo(stack, "poped top value.");
+    displayMstackInfo(stack, "pop top value (LIFO)");
 
-    std::cout << "Passed stack operations tests.\n" << std::endl;
+    // Swap content of stack with a new empty stack
+    MutantStack<int> anotherStack;
+    displayMstackInfo(anotherStack, "Instanciating another empty stack");
+    stack.swap(anotherStack);
+    displayMstackInfo(anotherStack, "Swapping content from original stack to new stack:");
+    displayMstackInfo(stack, "original stack:");
 }
 
 // Function to test deque characteristics
 void testDequeCharacteristics() {
     MutantStack<int> stack;
-    std::cout << "Testing deque characteristics via iterators..." << std::endl;
+    std::cout << YELLOW << "\nTesting deque characteristics via iterators..." << RESET << std::endl;
 
     // Push elements
     stack.push(1);
@@ -52,26 +59,39 @@ void testDequeCharacteristics() {
 
     // Use iterator to access elements like a deque
     int i = 1;
-    std::cout << "Using iterator to access element like a deque from beginning to end: ";
+    std::cout << "Mutant iterator displays:\t\t\t";
     for (auto it = stack.begin(); it != stack.end(); ++it, ++i) {
         std::cout << *it << " ";
+        if (it + 1 == stack.end()) {
+            *it = 42;
+            std::cout << std::endl;
+            displayMstackInfo(stack, "changed last iterated value via iterator");
+        }
     }
-    std::cout << std::endl;
 
     // Use reverse iterator
     auto rit = stack.rbegin();
-    std::cout << "Using reverse iterator set at rbegin() to last element like a deque: " << *rit << std::endl;
+    std::cout << "\nMutant reverse iterator points at \"end\":\t" << *rit << std::endl;
     ++rit;
-    std::cout << "Incrementing reverse iterator set originally at rbegin(): " << *rit << std::endl;
-
+    std::cout << "Incrementing reverse iterator at rbegin() + 1:\t" << *rit << std::endl;
     displayMstackInfo(stack, "stack after being iterated through");
-    std::cout << "Passed deque characteristics tests.\n" << std::endl;
+
+    // Use const_iterator to read and verify contents
+    std::cout << "Testing const_iterator: ";
+    for (MutantStack<int>::const_iterator it = stack.begin(); it != stack.end(); ++it) {
+        std::cout << *it << " ";  // Should output: 10 20 30
+        // *it = 100; // Uncommenting this line should cause a compilation error
+    }
+    std::cout << std::endl;
 }
 
-
 int main() {
-    testStackOperations();
-    testDequeCharacteristics();
+    try {
+        testStackOperations();
+        testDequeCharacteristics();
+    } catch (const std::exception& e) {
+        std::cout << RED << "Exception: " << e.what() << RESET << std::endl;
+    }
 
     return 0;
 }
