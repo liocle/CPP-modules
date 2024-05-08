7e71a3e755a8cf1bb452f62df240dc627cb95cd5
Lionel Clerc <lionel.clerc@protonmail.com>
2024-05-08 19:21:36 +0300

    Module09_Ex02: Fix empty string as input

--- Module_09/ex02/main.cpp
+++ Module_09/ex02/main.cpp
@@ -15,7 +15,7 @@ bool isSorted(const T& data) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
+    if (argc < 2 || (argc == 2 && *argv[1] == '\0')) {
         std::cout << RED << "Error: No numbers provided.\n" << RESET;
         return 1;
     }
